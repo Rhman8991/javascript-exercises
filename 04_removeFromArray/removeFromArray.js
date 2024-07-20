d444a18cfeadf877ea8093952c9a497928ff4db7
rahman <rahman17wr@gmail.com>
2024-07-20 19:39:50 +0700

    Remove multiple occurrences of values from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const arr = [...array]
+    for (let el of args) {
+        let index = arr.indexOf(el)
+        while (index !== -1) {
+            arr.splice(index, 1)
+            index = arr.indexOf(el)
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
