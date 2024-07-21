4f812fc87a3290f376bab60372417f6fbc231c33
rahman <rahman17wr@gmail.com>
2024-07-21 15:16:42 +0700

    Add functionality to the function, now it took two number as input and then return every integer between the inputed number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    const minNumber = Math.min(firstNumber, secondNumber);
+    const maxNumber = Math.max(firstNumber, secondNumber);
+    
+    if (
+        minNumber < 0 ||
+        typeof firstNumber !== "number" ||
+        typeof secondNumber !== "number" ||
+        !Number.isInteger(firstNumber) ||
+        !Number.isInteger(secondNumber) 
+    ) {
+        return 'ERROR'
+    }
 
+    const numberArray = []
+    for (let i = minNumber; i <= maxNumber; i++) {
+        numberArray.push(i)
+    }
+
+    return numberArray.reduce((sum, element) => sum += element)
 };
 
 // Do not edit below this line
