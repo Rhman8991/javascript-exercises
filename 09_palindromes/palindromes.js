d4d929b32b2b299b952b2aeb53585abdc3eb40fa
rahman <rahman17wr@gmail.com>
2024-07-23 09:45:11 +0700

    Implement palindrome function to check if given string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const regex = /[a-z0-9]/g;
+    const cleanReversedString = string.toLowerCase().match(regex).reverse().join("");
+    const cleanInputString = string.toLowerCase().match(regex).join("");
+    return cleanInputString === cleanReversedString ? true : false;
 };
 
 // Do not edit below this line
