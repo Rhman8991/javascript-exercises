c894acd7758488f97c540ef5838ed0c4d8550d18
rahman <rahman17wr@gmail.com>
2024-07-23 11:04:23 +0700

    Add Fibonacci function with error handling for negative inputs and type conversion for numeric values

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = Number(number);
+   if (number < 0) {
+    return "OOPS"
+   } else if (number === 0) {
+    return 0
+   }
+   let prevNum = 0;
+   let currNum = 1; 
+   for(let i = 1; i < number; i++) {
+    let nextNum = currNum + prevNum;
+    prevNum = currNum;
+    currNum = nextNum
+   }
+   return currNum
 };
 
 // Do not edit below this line
