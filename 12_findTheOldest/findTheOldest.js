986d0a670c18e2fb3a7f82dff37a5c39bf2bd899
rahman <rahman17wr@gmail.com>
2024-07-24 21:42:39 +0700

    Imlement function to return oldest people from the given array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    const currentYear = new Date().getFullYear();
+    const oldest = peopleArray.reduce((oldest, person) => {
+        const oldestAge = (oldest.yearOfDeath ? oldest.yearOfDeath : currentYear) - oldest.yearOfBirth;
+        const personAge = (person.yearOfDeath ? person.yearOfDeath : currentYear) - person.yearOfBirth;
+        return oldestAge > personAge ? oldest : person;
+    })
+    return oldest
 };
 
 // Do not edit below this line
